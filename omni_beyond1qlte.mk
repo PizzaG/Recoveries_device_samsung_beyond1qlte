f17f378d9a28a3b381735de3378e96c1de70705e
PizzaG <49663808+PizzaG@users.noreply.github.com>
2021-04-22 18:54:38 -0500

    Add files via upload

---
+++ omni_beyond1qlte.mk
@@ -0,0 +1,28 @@
+# Release name
+PRODUCT_RELEASE_NAME := beyond1qlte
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit Telephony packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit language packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/beyond1qlte/recovery/root,recovery/root)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := omni_beyond1qlte
+PRODUCT_DEVICE := beyond1qlte
+PRODUCT_MODEL := SM-G9730
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
